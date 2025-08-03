076e7a03c0b35f787cc7608e628b4886b34a5e64
Darkoakboat2121 <fornoahgaming@gmail.com>
2025-08-03 13:07:41 -0500

    Test

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 import { Client, Events, GatewayIntentBits, REST, Routes } from "discord.js"
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 })
 
 const admins = [
     'Darkoakboat2121'
 ]
 
+const badWords = [
+    'test'
+]
+
 client.on(Events.MessageCreate, (evd) => {
-    if (admins.includes(evd.author.tag)) return
-    client.emit(Events.MessageCreate, 'test')
+    // if (admins.includes(evd.author.tag)) return
+    if (evd.content === badWords[0]) evd.delete()
 })
+
+client.login('BOT_KEY')
